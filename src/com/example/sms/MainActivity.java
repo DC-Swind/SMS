9bc2e190003fbdc684281858c5b017641871b95a
Swind <dc.swind@gmail.com>
2015-03-04 17:21:38 +0800

    第一版
    简易实现功能

--- src/com/example/sms/MainActivity.java
+++ src/com/example/sms/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sms;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
@@ -9,8 +10,11 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.*;
 
 public class MainActivity extends Activity {
@@ -26,12 +30,13 @@ public class MainActivity extends Activity {
       
     /**联系人名称**/  
     private ArrayList<String> mContactsName = new ArrayList<String>();  
+    public int totalContact;
     /**联系人头像**/  
     private ArrayList<String> mContactsNumber = new ArrayList<String>();  
     
 	EditText[] showListName = new EditText[1000];
 	EditText[] showListNumber = new EditText[1000];
-	RadioButton[] showListDelete = new RadioButton[1000];
+	CheckBox[] showListDelete = new CheckBox[1000];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +56,54 @@ public class MainActivity extends Activity {
 		});
 		*/
 		setContentView(R.layout.activity_main);
+		Button importContact = (Button)findViewById(R.id.act1);
+		importContact.setOnClickListener(new OnClickListener(){ 
+			    public void onClick(View v) {  
+			        if(R.id.act1 == v.getId()){  
+			        	getPhoneContact();
+			        	drawContactList();
+			        }  
+			    }  
+		});
 		
-		getPhoneContact();
-		
-		drawContactList();
+		Button confirmChange = (Button)findViewById(R.id.act3);
+		confirmChange.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				if (R.id.act3 == v.getId()){
+					EditText smsContent = (EditText)findViewById(R.id.sms);
+					for(int i=0; i<totalContact;i++) if (showListDelete[i].isChecked()){
+						sendSMS(showListNumber[i].getText().toString(),showListName[i].getText().toString()+","+smsContent.getText().toString());
+					}
+					
+				}
+			}
+		});
 		//EditText show = (EditText)findViewById(R.id.msg);
 		//show.setText("1+1=2".toCharArray(),0,5);
 		
 	}
-
+	protected String sendSMS(String phone_number,String sms_content){
+		if (!checkNumber()) return "号码不合规范";  
+		
+		SmsManager smsManager = SmsManager.getDefault();
+        if(sms_content.length() > 70) {
+             List<String> contents = smsManager.divideMessage(sms_content);
+             for(String sms : contents) {
+                 smsManager.sendTextMessage(phone_number, null, sms, null, null);
+             }
+        } else {
+             smsManager.sendTextMessage(phone_number, null, sms_content, null, null);
+        }
+        Toast.makeText(MainActivity.this, "消息发送完成", Toast.LENGTH_SHORT).show();
+        return "发送成功";
+	}
+	private boolean checkNumber() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 	protected void drawContactList() {
 		EditText show = (EditText)findViewById(R.id.contactList);
-		int totalContact = mContactsName.size();
+		totalContact = mContactsName.size();
 		String s = new String("总共导入 "+totalContact+" 个联系人");
 		show.setText(s.toCharArray(),0,s.length());
 		
@@ -71,7 +112,7 @@ public class MainActivity extends Activity {
 		for(int i=0; i<min(totalContact,1000); i++){
 			showListName[i] = new EditText(mContext);
 			showListNumber[i] = new EditText(mContext);
-			showListDelete[i] = new RadioButton(mContext);
+			showListDelete[i] = new CheckBox(mContext);
 			String name = mContactsName.get(i);
 			showListName[i].setText(name.toCharArray(),0,name.length());
 			String number = mContactsNumber.get(i);
